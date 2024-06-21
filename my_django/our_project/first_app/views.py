4e7c4268a8efb52f64076c26f468ade978e3c191
FriesX <edgarchuang343@gmail.com>
2024-06-21 21:44:13 +0700

    Updates: command
    deleting some command

--- my_django/our_project/first_app/views.py
+++ my_django/our_project/first_app/views.py
@@ -28,16 +28,13 @@ def signin(request):
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            # Authentication successful
             login(request, user)
             print(request, user,password)
-            return redirect('index')  # Redirect to the home page or any other success page
+            return redirect('index')  
         else:
-            # Authentication failed
             error_message = 'Invalid username or password'
             return render(request, 'integran/signin.html', {'error_message': error_message})
     else:
-        # Render the signin template if the request method is GET
         return render(request, 'integran/signin.html')
       
 def register(request):
@@ -46,33 +43,25 @@ def register(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
         if username and email and password:
-            # Check if a user with the same username already exists
             if User.objects.filter(username=username).exists():
                 error_message = 'Username already exists'
                 return render(request, 'integran/register.html', {'error_message': error_message})
             else:
-                # Create a new user
                 user = User.objects.create_user(username=username, email=email, password=password)
                 if user is not None:
-                    # Authenticate the new user
                     user = authenticate(request, username=username, password=password)
                     if user is not None:
-                        # Authentication successful
                         login(request, user)
                         print(request, user, password)
-                        return redirect('index')  # Redirect to the home page or any other success page
+                        return redirect('index')  
                     else:
-                        # Authentication failed
                         error_message = 'Authentication failed'
                         return render(request, 'integran/register.html', {'error_message': error_message})
                 else:
-                    # User creation failed
                     error_message = 'User creation failed'
                     return render(request, 'integran/register.html', {'error_message': error_message})
         else:
-            # Missing form data
             error_message = 'Please fill in all the fields'
             return render(request, 'integran/register.html', {'error_message': error_message})
     else:
-        # Render the register template if the request method is GET
         return render(request, 'integran/register.html')
